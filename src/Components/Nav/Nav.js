ebd2d6b6057cfcaba4b8ca9fe6e655848f66389f
SivaAvi5 <avissiva229@gmail.com>
2024-10-01 17:48:28 +0530

    fix:nav icon and styles are changed

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,35 +1,42 @@
-import React, { useState } from 'react'
-import './Nav.css'
-import { NavLink } from 'react-router-dom'
-import Logo from '../../logo/logo.png'
+import React, { useState } from "react";
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
+import Logo from "../../logo/logo.png";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { GrFormClose } from "react-icons/gr"
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
-    <div className='nav'>
-      <div className='nav__container'>
-        <NavLink to='/' className='links' activeClassName='active'>
-          <img src={Logo} alt='logo'/>
-        </NavLink> 
-        <div className={`nav__btns ${isOpen ? 'open' : ''}`}>
-          <NavLink exact to='/' className='links' activeClassName='active'><span>Home</span></NavLink>
-          <NavLink to='/skills' className='links' activeClassName='active'><span>Skills</span></NavLink>
-          <NavLink to='/projects' className='links' activeClassName='active'><span>Projects</span></NavLink>
-          <NavLink to='/contact' className='links' activeClassName='active'><span>Contact</span></NavLink> 
+    <div className="nav">
+      <div className="nav__container">
+        <NavLink to="/" className="links" activeClassName="active">
+          <img src={Logo} alt="logo" />
+        </NavLink>
+        <div className={`nav__btns ${isOpen ? "open" : ""}`}>
+          <NavLink exact to="/" className="links" activeClassName="active">
+            <span>Home</span>
+          </NavLink>
+          <NavLink to="/skills" className="links" activeClassName="active">
+            <span>Skills</span>
+          </NavLink>
+          <NavLink to="/projects" className="links" activeClassName="active">
+            <span>Projects</span>
+          </NavLink>
+          <NavLink to="/contact" className="links" activeClassName="active">
+            <span>Contact</span>
+          </NavLink>
         </div>
-        <button className='burger' onClick={toggleMenu}>
-          <span className='burger__line'></span>
-          <span className='burger__line'></span>
-          <span className='burger__line'></span>
-        </button>
+        {isOpen ? (
+          <GrFormClose className="burger" onClick={() => setIsOpen(false)} />
+        ) : (
+            <HiMenuAlt1 className="burger" onClick={() => setIsOpen(true)}/>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
